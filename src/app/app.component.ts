b473ad32e8093b6b5019c0afa1ff263deb16c6f7
Diego Carciente <diego.carciente@gmail.com>
2017-06-09 00:13:01 -0400

    Menu Toogle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
+import { RedactarPage } from './../pages/redactar/redactar';
 import { InboxPage } from './../pages/inbox/inbox';
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, NavController, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -9,14 +10,27 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 })
 export class MyApp {
   rootPage:any = InboxPage;
+  @ViewChild('content') menu: NavController;
+  inboxPage = InboxPage;
+  redactarPage = RedactarPage
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform, 
+    statusBar: StatusBar, 
+    splashScreen: SplashScreen,
+    public menuController: MenuController
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+  
+  abrirPagina(pagina){
+    this.menu.setRoot(pagina);
+    this.menuController.close();
   }
 }
 
